3da9fb5c45ff839d8d9657f6c05ffd06bae4286c
umayer10-AI <mdumayer10@gmail.com>
2026-05-27 09:11:21 +0600

    add

--- dist/c1.ts
+++ dist/c1.ts
@@ -21,24 +21,39 @@
 // console.log(a.buyData())
 
 
+// class P {
+//     public isCart = false
+//     constructor(public name: string, private price:number, public id: number){}
+
+//     getData() {
+//         return this.price
+//     }
+
+//     buyData() {
+//         return this.getData()
+//     }
+// }
+// const a = new P("umayer", 10000, 10)
+// // a.name = "Ahmad"
+
+// // console.log(a.getData())
+// console.log(a.buyData())
+
+
 class P {
     public isCart = false
-    constructor(public name: string, public price:number, public id: number){}
+    constructor(public name: string, private price:number, public id: number){}
 
     getData() {
-        this.isCart = true
+        return this.price
     }
 
     buyData() {
-        if(this.isCart){
-            return `${this.name} takar malik holo ${this.price}`
-        }
-        else{
-            return "Process not available"
-        }
+        return this.getData()
     }
 }
 const a = new P("umayer", 10000, 10)
 // a.name = "Ahmad"
-a.getData()
+
+// console.log(a.getData())
 console.log(a.buyData())
