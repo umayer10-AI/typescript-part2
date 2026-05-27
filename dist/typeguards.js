d0c7cbfb3a3075d586f1a30ed43d1fb7c1aab266
umayer10-AI <mdumayer10@gmail.com>
2026-05-27 12:36:15 +0600

    add

--- dist/typeguards.js
+++ dist/typeguards.js
@@ -1,3 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+let t = "Umayer";
+// t = true
+if (typeof t === "boolean") {
+    console.log("Boolean");
+}
+else if (typeof t === "string") {
+    console.log("String");
+}
+else {
+    console.log("Number");
+}
 //# sourceMappingURL=typeguards.js.map
---
+++ typeguards.ts
@@ -0,0 +1,11 @@
+let t:string | number | boolean = "Umayer"
+// t = true
+if(typeof t==="boolean"){
+    console.log("Boolean")
+}
+else if(typeof t==="string"){
+    console.log("String")
+}
+else{
+    console.log("Number")
+}
