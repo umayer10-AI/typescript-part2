fc3a5cc02dd8b308e605260ae056cdc4d2708025
umayer10-AI <mdumayer10@gmail.com>
2026-05-27 09:07:20 +0600

    add

--- dist/c1.ts
+++ dist/c1.ts
@@ -1,3 +1,26 @@
+// class P {
+//     public isCart = false
+//     constructor(public name: string, public price:number, public id: number){}
+
+//     getData() {
+//         this.isCart = true
+//     }
+
+//     buyData() {
+//         if(this.isCart){
+//             return `${this.name} takar malik holo ${this.price}`
+//         }
+//         else{
+//             return "Process not available"
+//         }
+//     }
+// }
+// const a = new P("umayer", 10000, 10)
+// // a.name = "Ahmad"
+// a.getData()
+// console.log(a.buyData())
+
+
 class P {
     public isCart = false
     constructor(public name: string, public price:number, public id: number){}
