750d6eac6ad0d37001a2aa75a18e5cd83087c676
umayer10-AI <mdumayer10@gmail.com>
2026-05-27 10:55:39 +0600

    add

---
+++ data.ts
@@ -0,0 +1,6 @@
+class A {
+    constructor(public name: string){}
+}
+
+const a = new A("Umayer")
+console.log(a.name)
---
+++ dist/data.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class A {
+    name;
+    constructor(name) {
+        this.name = name;
+    }
+}
+const a = new A("Umayer");
+console.log(a.name);
+//# sourceMappingURL=data.js.map
