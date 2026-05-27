bc259d26bbc726ff172e49390a22981691671c18
umayer10-AI <mdumayer10@gmail.com>
2026-05-27 12:28:11 +0600

    add

--- dist/static.js
+++ dist/static.js
@@ -2,8 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 class A {
     static name = "Hello";
+    static getName() {
+        // return "Umayer"
+        return this.name;
+    }
 }
 const a = new A();
 // console.log(a.name)
 console.log(A.name);
+console.log(A.getName());
 //# sourceMappingURL=static.js.map
--- static.ts
+++ static.ts
@@ -1,6 +1,12 @@
 class A {
     static name: string = "Hello"
+
+    static getName() {
+        // return "Umayer"
+        return this.name
+    }
 }
 const a = new A()
 // console.log(a.name)
 console.log(A.name)
+console.log(A.getName())
