3fbf4c9f9f1010bcd4b0c16a562678179825681a
umayer10-AI <mdumayer10@gmail.com>
2026-05-27 10:45:59 +0600

    add

--- dist/get_set.js
+++ dist/get_set.js
@@ -2,10 +2,15 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 class A {
     _name = "Umayer";
+    _email = "Umayer@gmail";
     get name() {
         return "MR. " + this._name;
+    }
+    get email() {
+        return "MR. " + this._email;
     }
 }
 const a = new A();
 console.log(a.name);
+console.log(a.email);
 //# sourceMappingURL=get_set.js.map
--- get_set.ts
+++ get_set.ts
@@ -1,11 +1,16 @@
 class A {
     public _name: string = "Umayer"
+    public _email: string = "Umayer@gmail"
 
     get name(): string {
         return "MR. "+ this._name
+    }
+    get email() : string {
+        return "MR. "+this._email
     }
 
 }
 
 const a = new A()
 console.log(a.name)
+console.log(a.email)
