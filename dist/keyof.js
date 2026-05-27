2817bd0f51f65239046920d4252cfd5760d29b08
umayer10-AI <mdumayer10@gmail.com>
2026-05-27 13:27:23 +0600

    add

---
+++ dist/keyof.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let a = {
+    name: "Umayer",
+    age: 21,
+    free: true,
+};
+console.log(a);
+//# sourceMappingURL=keyof.js.map
---
+++ keyof.ts
@@ -0,0 +1,11 @@
+type P = {
+    name: string,
+    age: number,
+    free: boolean
+}
+let a: P = {
+    name: "Umayer",
+    age: 21,
+    free: true,
+}
+console.log(a)
