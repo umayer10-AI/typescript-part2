84e7216ad46fa207cc3b08a291c354dd658e23da
umayer10-AI <mdumayer10@gmail.com>
2026-05-27 12:23:40 +0600

    add

--- dist/static.js
+++ dist/static.js
@@ -1,3 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+class A {
+    static name = "Hello";
+}
+const a = new A();
+// console.log(a.name)
+console.log(A.name);
 //# sourceMappingURL=static.js.map
---
+++ static.ts
@@ -0,0 +1,6 @@
+class A {
+    static name: string = "Hello"
+}
+const a = new A()
+// console.log(a.name)
+console.log(A.name)
