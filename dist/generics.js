a280fa39d7bed237d4997a88ed5b108659a7a392
umayer10-AI <mdumayer10@gmail.com>
2026-05-27 13:22:25 +0600

    add

--- dist/generics.js
+++ dist/generics.js
@@ -1,9 +1,14 @@
 "use strict";
+// const a = <T>(name: T): T => {
+//     return name
+// }
+// console.log(a("Hello"))
+// console.log(a<number>(100))
+// console.log(a(true))
 Object.defineProperty(exports, "__esModule", { value: true });
-const a = (name) => {
-    return name;
+const a = (v) => {
+    return v;
 };
-console.log(a("Hello"));
-console.log(a(100));
-console.log(a(true));
+const b = a([1, 2, 3, 4, 5]);
+console.log(b);
 //# sourceMappingURL=generics.js.map
--- generics.ts
+++ generics.ts
@@ -1,6 +1,12 @@
-const a = <T>(name: T): T => {
-    return name
+// const a = <T>(name: T): T => {
+//     return name
+// }
+// console.log(a("Hello"))
+// console.log(a<number>(100))
+// console.log(a(true))
+
+const a = <T>(v: T): T => {
+    return v
 }
-console.log(a("Hello"))
-console.log(a<number>(100))
-console.log(a(true))
+const b = a([1,2,3,4,5])
+console.log(b)
